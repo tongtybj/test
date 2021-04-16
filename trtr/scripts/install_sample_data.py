6c594dab28576e0b1cd06e6a61a7fc499fec1a5b
Moju Zhao <chou@jsk.imi.i.u-tokyo.ac.jp>
2021-04-17 01:06:52 +0900

    Add sample launch file to perform trtr tracker

--- trtr/scripts/install_sample_data.py
+++ trtr/scripts/install_sample_data.py
@@ -5,7 +5,7 @@ from jsk_data import download_data
 def main():
     PKG = 'trtr'
 
-    ### KiteSurf
+    ### OTB100 KiteSurf
     download_data(
         pkg_name=PKG,
         path='test_data/KiteSurf.zip',
@@ -14,7 +14,7 @@ def main():
         extract=True,
     )
 
-    ### Biker
+    ### OTB100 Biker
     download_data(
         pkg_name=PKG,
         path='test_data/Biker.zip',
@@ -22,6 +22,19 @@ def main():
         md5='0c05d3d3ba6d450e8c7a8a9b304f86af',
         extract=True,
     )
+
+    ### rosbag of jsk_perception
+    download_data(
+        pkg_name=PKG,
+        path='sample/data/2016-10-15-23-21-42_moving_bottle.bag',
+        url='https://drive.google.com/uc?id=0B9P1L--7Wd2vTDZOZGhrTTNvc00',
+        md5='ad4e7d298c0d9985295d93e47c7b03e6',
+        extract=False,
+        compressed_bags=[
+            'sample/data/2016-10-15-23-21-42_moving_bottle.bag',
+        ],
+    )
+
 
 if __name__ == '__main__':
     main()
